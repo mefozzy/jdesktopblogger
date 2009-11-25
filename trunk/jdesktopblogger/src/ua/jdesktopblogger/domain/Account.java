4ce3beb0a06ff35f919f26e71005766f17471497
AlexSkosyr <AlexSkosyr@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-25 12:48:05 +0000

    added JAXB annotations

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/domain/Account.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/domain/Account.java
@@ -2,25 +2,39 @@ package ua.jdesktopblogger.domain;
 
 import java.util.Set;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
 
 /**
  * Represents a single account that is stored on the
  * user machine. Account holds auth information and collection of blogs.
  * 
- * @author Yuriy Tkach  
+ * @author Yuriy Tkach
  */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name="account", propOrder= { "name", "login", "password" } )
 public class Account {
 
 	/** Name of the account */
+	@XmlElement(required=true)
 	private String name;
 	
 	/** User login */
+	@XmlElement(required=true)
 	private String login;
 	
 	/** User password */
+	@XmlElement(required=true)
 	private String password;
 	
 	/** Collection of account blogs */
+	@XmlTransient
 	private Set<Blog> blogs;
 
 	/**
