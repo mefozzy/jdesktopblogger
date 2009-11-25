08b9942126b64e7b57395a2382d75432348e56ef
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-25 12:09:24 +0000

    Added reaction on buttons, added popups for text-fields

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/ui/AccountDialog.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/ui/AccountDialog.java
@@ -2,14 +2,24 @@ package ua.jdesktopblogger.ui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import ua.cn.yet.common.ui.popup.PopupFactory;
+import ua.cn.yet.common.ui.popup.PopupListener;
 
 import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.ColumnSpec;
@@ -24,15 +34,23 @@ public class AccountDialog extends JDialog {
 	private JTextField textField;
 	private JPasswordField passwordField;
 
+	private JButton okButton;
+
+	private JButton cancelButton;
+
 	/**
 	 * Create the dialog.
 	 */
-	public AccountDialog() {
+	public AccountDialog(JFrame owner) {
+		super(owner);
 		setModal(true);
 		setResizable(false);
 		setTitle("Account information");
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setBounds(100, 100, 450, 145);
+		setLocationRelativeTo(owner);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -45,39 +63,89 @@ public class AccountDialog extends JDialog {
 				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC, }));
-		{
-			JLabel lblLogin = new JLabel("Login:");
-			contentPanel.add(lblLogin, "2, 2, right, default");
-		}
-		{
-			textField = new JTextField();
-			contentPanel.add(textField, "4, 2, fill, default");
-			textField.setColumns(10);
-		}
-		{
-			JLabel lblPassword = new JLabel("Password:");
-			contentPanel.add(lblPassword, "2, 4, right, default");
-		}
-		{
-			passwordField = new JPasswordField();
-			contentPanel.add(passwordField, "4, 4, fill, default");
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			contentPanel.add(buttonPane, "4, 6");
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+
+		addKeyListener(new KeyAdapter() {
+			public void keyReleased(final KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+					cancelButton.doClick();
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+					okButton.doClick();
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+		});
+
+		MouseListener popupListener = new PopupListener(PopupFactory
+				.getEditPopup());
+
+		JLabel lblLogin = new JLabel("Login:");
+		contentPanel.add(lblLogin, "2, 2, right, default");
+
+		textField = new JTextField();
+		contentPanel.add(textField, "4, 2, fill, default");
+		textField.setColumns(10);
+		textField.addKeyListener(getKeyListeners()[0]);
+		textField.addMouseListener(popupListener);
+
+		lblLogin.setLabelFor(textField);
+
+		JLabel lblPassword = new JLabel("Password:");
+		contentPanel.add(lblPassword, "2, 4, right, default");
+
+		passwordField = new JPasswordField();
+		passwordField.addKeyListener(getKeyListeners()[0]);
+		passwordField.addMouseListener(popupListener);
+		contentPanel.add(passwordField, "4, 4, fill, default");
+
+		lblPassword.setLabelFor(passwordField);
+
+		JPanel buttonPane = new JPanel();
+		contentPanel.add(buttonPane, "4, 6");
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+		okButton = new JButton("OK");
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		//okButton.addKeyListener(getKeyListeners()[0]);
+		okButton.setMnemonic(KeyEvent.VK_ACCEPT);
+		getRootPane().setDefaultButton(okButton);
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				rememberAccountInfo();
 			}
+		});
+
+		cancelButton = new JButton("Cancel");
+		//cancelButton.addKeyListener(getKeyListeners()[0]);
+		cancelButton.setMnemonic(KeyEvent.VK_CANCEL);
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				dispose();
+			}
+		});
+		cancelButton.setText("Cancel");
+		buttonPane.add(cancelButton);
+
+	}
+
+	/**
+	 * Remembering account information and closing dialog if successful
+	 */
+	protected void rememberAccountInfo() {
+		if (textField.getText().isEmpty()) {
+			JOptionPane.showMessageDialog(this,
+					"Account login cannot be empty", this.getTitle(),
+					JOptionPane.ERROR_MESSAGE);
+			return;
 		}
+		if (passwordField.getPassword().length == 0) {
+			JOptionPane.showMessageDialog(this,
+					"Account password cannot be empty", this.getTitle(),
+					JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+
+		// TODO: Add account saving code
+		JOptionPane.showMessageDialog(this, "Save account action");
+		dispose();
 	}
 
 }
