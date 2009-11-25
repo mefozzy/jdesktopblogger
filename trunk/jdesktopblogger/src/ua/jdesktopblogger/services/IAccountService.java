e747b1ceff6b5b03f19d7ae730d1e5654d0e4e5c
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-25 13:54:59 +0000

    Changed to throw AccountIOException on error for all methods

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/services/IAccountService.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/services/IAccountService.java
@@ -1,10 +1,7 @@
 package ua.jdesktopblogger.services;
 
-import java.io.FileNotFoundException;
-
-import javax.xml.bind.JAXBException;
-
 import ua.jdesktopblogger.domain.Account;
+import ua.jdesktopblogger.excetions.AccountIOException;
 
 /**
  * Interface with function to manage Blog's entities
@@ -17,17 +14,15 @@ public interface IAccountService {
 	/**
 	 * Load default account from user home directory (if it exists) or null otherwise
 	 * @param login - login to get account info 
-	 * @throws JAXBException 
-	 * @throws FileNotFoundException 
+	 * @throws AccountIOException If io error occurs
 	 */
-	public Account getLoadedAccount(String login) throws JAXBException, FileNotFoundException;
+	public Account getLoadedAccount(String login) throws AccountIOException;
 	
 	/**
 	 * Save a specified account to the file in the user home directory
 	 * @param account - account to save
-	 * @throws JAXBException
-	 * @throws FileNotFoundException 
+	 * @throws AccountIOException If io error occurs 
 	 */
-	public void saveAccount(Account account) throws JAXBException, FileNotFoundException ;
+	public void saveAccount(Account account) throws AccountIOException;
 	
 }
