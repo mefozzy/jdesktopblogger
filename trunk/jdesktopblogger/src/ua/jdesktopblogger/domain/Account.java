f2f388810490f986d0a7969fdb4ce6e1f56b5abf
AlexSkosyr <AlexSkosyr@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-26 13:49:59 +0000

    done encryption/decryption password

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/domain/Account.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/domain/Account.java
@@ -1,15 +1,11 @@
 package ua.jdesktopblogger.domain;
 
 import java.security.Key;
-import java.security.MessageDigest;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -17,45 +13,49 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
-import ua.jdesktopblogger.utils.Base64Coder;
-import sun.misc.*;
-
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
- * Represents a single account that is stored on the
- * user machine. Account holds auth information and collection of blogs.
+ * Represents a single account that is stored on the user machine. Account holds
+ * auth information and collection of blogs.
  * 
  * @author Yuriy Tkach
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name="account", propOrder= { "id", "login", "password" } )
+@XmlType(name = "account", propOrder = { "id", "login", "password" })
 public class Account {
-
-	private static final String ENCODE_WORD = "bambarbiya";
+	
+	private static final String ALGORITHM = "AES";
+	
+    private static final byte[] keyValue = 
+        new byte[] { 'T', 'h', 'i', 's', 'I', 's', 'A', 'S', 'e', 'c', 'r', 'e', 't', 'K', 'e', 'y' };
 
 	/** Name of the account */
-	@XmlElement(required=true)
+	@XmlElement(required = true)
 	private String id;
 	
-	/** 
-	 * Blog provider access object that is used by blog provider
-	 * to store its connection or some other needed information.
-	 * For every method call of blog provider the account object
-	 * is passed, so blog provider can store and then access
-	 * some information in this object. 
+	@XmlTransient
+	private boolean encrypt = false;
+
+	/**
+	 * Blog provider access object that is used by blog provider to store its
+	 * connection or some other needed information. For every method call of
+	 * blog provider the account object is passed, so blog provider can store
+	 * and then access some information in this object.
 	 */
 	@XmlTransient
 	private Object providerObject;
-	
+
 	/** User login */
-	@XmlElement(required=true)
+	@XmlElement(required = true)
 	private String login;
-	
+
 	/** User password */
-	@XmlElement(required=true)
+	@XmlElement(required = true)
 	private String password;
-	
+
 	/** Collection of account blogs */
 	@XmlTransient
 	private Set<Blog> blogs = new HashSet<Blog>();
@@ -68,7 +68,8 @@ public class Account {
 	}
 
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(String id) {
 		this.id = id;
@@ -82,7 +83,8 @@ public class Account {
 	}
 
 	/**
-	 * @param login the login to set
+	 * @param login
+	 *            the login to set
 	 */
 	public void setLogin(String login) {
 		this.login = login;
@@ -96,7 +98,8 @@ public class Account {
 	}
 
 	/**
-	 * @param password the password to set
+	 * @param password
+	 *            the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
@@ -110,68 +113,63 @@ public class Account {
 	}
 
 	/**
-	 * @param blogs the blogs to set
+	 * @param blogs
+	 *            the blogs to set
 	 */
 	public void setBlogs(Set<Blog> blogs) {
 		this.blogs = blogs;
 	}
 
-	public byte[] encryptString(String stringToEncrypt){
-		byte[] res = null;
+	public void encryptPasswd(){
+		if (encrypt == true){
+    		return;
+    	}
+		String encryptedValue = null;
 		try {
-			Cipher cipher = Cipher.getInstance("DES");
-			Key key = KeyGenerator.getInstance("DES", "hex").generateKey();
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-	        byte[] inputBytes = stringToEncrypt.getBytes();
-	        res = cipher.doFinal(inputBytes);
-	        
-			
-//			DESKeySpec keySpec = new DESKeySpec(ENCODE_WORD.getBytes("UTF8"));
-//			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-//			SecretKey key = keyFactory.generateSecret(keySpec);
-//			sun.misc.BASE64Encoder base64encoder = new BASE64Encoder();
-//		
-//			
-//			// ENCODE plainTextPassword String
-//			byte[] cleartext = stringToEncrypt.getBytes("UTF8");      
-//
-//			Cipher cipher = Cipher.getInstance("DES"); // cipher is not thread safe
-//			cipher.init(Cipher.ENCRYPT_MODE, key);
-//			res = base64encoder.encode(resByteArr);
+	        Key key = generateKey();
+	        Cipher c = Cipher.getInstance(ALGORITHM);
+	        c.init(Cipher.ENCRYPT_MODE, key);
+	        byte[] encValue = c.doFinal(this.password.getBytes());
+	        encryptedValue = new BASE64Encoder().encode(encValue);
 		} catch (Exception e){
 			e.printStackTrace();
-			res = null;
+			return;
 		}
-		return res;
-	}
+		encrypt = true;
+        password = encryptedValue;
+    }
 	
-	public String decryptString(byte[] stringToDecrypt){
-		String res = null;
-		try {
-			Cipher cipher = Cipher.getInstance("DES");
-			Key key = KeyGenerator.getInstance("DES", "hex").generateKey();
-			cipher.init(Cipher.DECRYPT_MODE, key);
-			byte[] resBytes = cipher.doFinal(stringToDecrypt);
-	        res = new String(resBytes);
-			
-//			DESKeySpec keySpec = new DESKeySpec("Your secret Key phrase".getBytes("UTF8"));
-//			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-//			SecretKey key = keyFactory.generateSecret(keySpec);
-//	
-//			// DECODE encryptedPwd String
-//
-//			Cipher cipher = Cipher.getInstance("DES");// cipher is not thread safe
-//			cipher.init(Cipher.DECRYPT_MODE, key);
-//			byte[] plainTextPwdBytes = (cipher.doFinal(encrypedPwdBytes));
-//
-//			res = new String(plainTextPwdBytes);
-		} catch (Exception e){
-			e.printStackTrace();
-			res = null;
-		}
-		return res;
-	}
-
+    public void decryptPasswd() {
+    	if (encrypt == false){
+    		return;
+    	}
+    	String decryptedValue = null;
+    	try {
+	        Key key = generateKey();
+	        Cipher c = Cipher.getInstance(ALGORITHM);
+	        c.init(Cipher.DECRYPT_MODE, key);
+	        byte[] decordedValue = new BASE64Decoder().decodeBuffer(this.password);
+	        byte[] decValue = c.doFinal(decordedValue);
+	        decryptedValue = new String(decValue);
+    	} catch (Exception e){
+    		e.printStackTrace();
+    		return;
+    	}
+    	encrypt = false;
+        password = decryptedValue;
+    }
+    
+    private static Key generateKey() {
+    	Key key = null;
+    	try {
+	        key = new SecretKeySpec(keyValue, ALGORITHM);
+	        // SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ALGORITHM);
+	        // key = keyFactory.generateSecret(new DESKeySpec(keyValue));
+    	} catch (Exception e){
+    		e.printStackTrace();
+    	}
+        return key;
+    }
 	/**
 	 * @return the providerObject
 	 */
@@ -180,11 +178,11 @@ public class Account {
 	}
 
 	/**
-	 * @param providerObject the providerObject to set
+	 * @param providerObject
+	 *            the providerObject to set
 	 */
 	public void setProviderObject(Object providerObject) {
 		this.providerObject = providerObject;
 	}
-
 
 }
