b199a682690047e3c4354fda36b50a2e9e23ed8b
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-27 13:26:18 +0000

    Added loading of posts for blog

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/services/impl/BlogServiceImpl.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/services/impl/BlogServiceImpl.java
@@ -1,7 +1,9 @@
 package ua.jdesktopblogger.services.impl;
 
 import ua.jdesktopblogger.domain.Account;
+import ua.jdesktopblogger.domain.Blog;
 import ua.jdesktopblogger.domain.IAccountListener;
+import ua.jdesktopblogger.domain.IPostListener;
 import ua.jdesktopblogger.excetions.BlogServiceException;
 import ua.jdesktopblogger.providers.IBlogProvider;
 import ua.jdesktopblogger.providers.ProviderFactory;
@@ -27,6 +29,25 @@ public class BlogServiceImpl implements IBlogService {
 		account.setBlogs(pr.loadListOfBlogs(account));
 		
 		accountListener.accountRefreshed(account);
+	}
+
+	/* (non-Javadoc)
+	 * @see ua.jdesktopblogger.services.IBlogService#loadPosts(ua.jdesktopblogger.domain.Account, ua.jdesktopblogger.domain.Blog)
+	 */
+	@Override
+	public void loadPosts(Account account, Blog blog, IAccountListener accountListener, IPostListener postListener)
+			throws BlogServiceException {
+		
+		IBlogProvider pr = ProviderFactory.getBlogProvider(account);
+		
+		if (account.getProviderObject() == null) {
+			refreshAccount(account, accountListener);
+			throw new BlogServiceException("Please, select blog to load posts for.");
+		}
+		
+		blog.setPosts(pr.loadListOfPosts(account, blog));
+		
+		postListener.postsLoaded(blog);
 	}
 
 }
