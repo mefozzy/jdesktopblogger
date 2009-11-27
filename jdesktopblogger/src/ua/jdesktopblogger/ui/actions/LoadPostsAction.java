d32396574f9dcc5a5f75bb4e17c7bd6ec499c095
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-27 13:30:04 +0000

    Added

---
+++ jdesktopblogger/src/ua/jdesktopblogger/ui/actions/LoadPostsAction.java
@@ -0,0 +1,65 @@
+package ua.jdesktopblogger.ui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+
+import ua.jdesktopblogger.Messages;
+import ua.jdesktopblogger.domain.Account;
+import ua.jdesktopblogger.domain.Blog;
+import ua.jdesktopblogger.excetions.BlogServiceException;
+import ua.jdesktopblogger.services.ServiceFactory;
+import ua.jdesktopblogger.ui.MainForm;
+
+@SuppressWarnings("serial")//$NON-NLS-1$
+public class LoadPostsAction extends SuperAction {
+
+	public LoadPostsAction(MainForm fr) {
+		this("Load posts", MainForm.createImageIcon("images/mail_get.png"), //$NON-NLS-1$
+				KeyEvent.VK_E, KeyStroke.getKeyStroke(KeyEvent.VK_E,
+						ActionEvent.ALT_MASK), "Load posts for the blog", fr);
+
+		setEnabled(false);
+	}
+
+	public LoadPostsAction(String caption, ImageIcon icon, int keyEvent,
+			KeyStroke keyStroke, String description, MainForm fr) {
+		super(caption, icon, keyEvent, keyStroke, description, fr);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+
+		Account account = form.getSelectedAccount();
+		if (account != null) {
+
+			Blog blog = form.getSelectedBlog();
+
+			if (blog != null) {
+
+				try {
+					ServiceFactory.getDefaultFactory().getBlogService()
+							.loadPosts(account, blog, form, form);
+				} catch (BlogServiceException e) {
+					e.printStackTrace();
+					JOptionPane.showMessageDialog(form.getFrame(),
+							"Failed to load posts for blog."
+									+ Messages.NEW_LINE_DOUBLE
+									+ e.getLocalizedMessage(), form
+									.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+				}
+			} else {
+				JOptionPane.showMessageDialog(form.getFrame(),
+						"Please, select blog to load posts for", form
+								.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+			}
+		} else {
+			JOptionPane.showMessageDialog(form.getFrame(),
+					"Please, select an account for refreshing", form
+							.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+}
