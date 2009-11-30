317eabe9b0e1e27400962d2821908ef2172e1c69
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-30 14:53:04 +0000

    Added calling of service and processing objects

--- trunk/jdesktopblogger/src/ua/jdesktopblogger/ui/actions/newpost/PublishPostAction.java
+++ trunk/jdesktopblogger/src/ua/jdesktopblogger/ui/actions/newpost/PublishPostAction.java
@@ -4,19 +4,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
+import ua.jdesktopblogger.Messages;
+import ua.jdesktopblogger.domain.Account;
+import ua.jdesktopblogger.domain.Blog;
+import ua.jdesktopblogger.domain.IAccountListener;
+import ua.jdesktopblogger.domain.IPostListener;
+import ua.jdesktopblogger.domain.Post;
+import ua.jdesktopblogger.excetions.BlogServiceException;
+import ua.jdesktopblogger.services.ServiceFactory;
 import ua.jdesktopblogger.ui.NewPostForm;
 
-@SuppressWarnings("serial") //$NON-NLS-1$
+/**
+ * Action for publishing new post
+ * 
+ * @author Yuriy Tkach
+ */
+@SuppressWarnings("serial")//$NON-NLS-1$
 public class PublishPostAction extends NewPostFormSuperAction {
 
-	public PublishPostAction(NewPostForm form) {
-		this("Publish",
-				null,
-				KeyEvent.VK_P, KeyStroke.getKeyStroke(KeyEvent.VK_ACCEPT,
-						ActionEvent.CTRL_MASK),
+	private Account account;
+	private IAccountListener accountListener;
+	private IPostListener postsListener;
+
+	public PublishPostAction(NewPostForm form, Account account,
+			IAccountListener accountListener, IPostListener postListener) {
+		this("Publish", null, KeyEvent.VK_P, KeyStroke.getKeyStroke(
+				KeyEvent.VK_ACCEPT, ActionEvent.CTRL_MASK),
 				"Publish post to the server", form);
+
+		this.account = account;
+		this.accountListener = accountListener;
+		this.postsListener = postListener;
 	}
 
 	public PublishPostAction(String caption, ImageIcon icon, int keyEvent,
@@ -24,11 +45,41 @@ public class PublishPostAction extends NewPostFormSuperAction {
 		super(caption, icon, keyEvent, keyStroke, description, form);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
 	public void actionPerformed(ActionEvent arg0) {
-		getNewPostForm().dispose();
+		Blog blog = getNewPostForm().getSelectedBlog();
+		if (blog != null) {
+
+			Post post = getNewPostForm().getEditedPost();
+
+			if (post != null) {
+
+				try {
+					ServiceFactory.getDefaultFactory().getBlogService()
+							.publishPost(account, blog, post, accountListener,
+									postsListener);
+
+					getNewPostForm().dispose();
+				} catch (BlogServiceException e) {
+					e.printStackTrace();
+
+					JOptionPane.showMessageDialog(form.getFrame(),
+							"Failed to publish post."
+									+ Messages.NEW_LINE_DOUBLE
+									+ e.getLocalizedMessage(), form
+									.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		} else {
+			JOptionPane.showMessageDialog(form.getFrame(),
+					"Please, select a blog to create new post", form
+							.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+		}
 	}
 
 }
