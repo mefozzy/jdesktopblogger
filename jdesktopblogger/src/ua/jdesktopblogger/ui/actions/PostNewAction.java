8f5c438e548bdd6602389d056b37740b65658a7b
yuriytkach <yuriytkach@779a80d4-d99d-11de-8314-133c20781edd>
2009-11-30 14:52:27 +0000

    Added params to constructor of the new post form

--- jdesktopblogger/src/ua/jdesktopblogger/ui/actions/PostNewAction.java
+++ jdesktopblogger/src/ua/jdesktopblogger/ui/actions/PostNewAction.java
@@ -4,20 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
+import ua.jdesktopblogger.domain.Account;
 import ua.jdesktopblogger.ui.MainForm;
 import ua.jdesktopblogger.ui.NewPostForm;
 
-@SuppressWarnings("serial") //$NON-NLS-1$
+/**
+ * Action for showing new post form
+ * 
+ * @author Yuriy Tkach
+ */
+@SuppressWarnings("serial")//$NON-NLS-1$
 public class PostNewAction extends SuperAction {
 
 	public PostNewAction(MainForm fr) {
-		this("New post",
-				MainForm.createImageIcon("images/new_post.png"), //$NON-NLS-1$
+		this("New post", MainForm.createImageIcon("images/new_post.png"), //$NON-NLS-1$
 				KeyEvent.VK_N, KeyStroke.getKeyStroke(KeyEvent.VK_N,
-						ActionEvent.CTRL_MASK),
-				"Create new post", fr);
+						ActionEvent.CTRL_MASK), "Create new post", fr);
 		setEnabled(false);
 	}
 
@@ -27,7 +32,16 @@ public class PostNewAction extends SuperAction {
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		new NewPostForm().setVisible(true);
+
+		Account account = form.getSelectedAccount();
+		if (account != null) {
+			new NewPostForm(form, account, form.getSelectedBlog())
+					.setVisible(true);
+		} else {
+			JOptionPane.showMessageDialog(form.getFrame(),
+					"Please, select an account or blog to create new post",
+					form.getAppTitle(), JOptionPane.ERROR_MESSAGE);
+		}
 	}
 
 }
